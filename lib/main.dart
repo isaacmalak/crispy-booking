20c28a687d286e958088ad1b8b1edd76ad4bad47
abdelrhman <abdelrhmanadel2512@gmail.com>
2024-09-26 20:45:42 +0300

    feet:reset_password_screens_2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+
+import 'package:crispy_booking/reset_password/forgot_password.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'reset_password/reset_password.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primaryColor: const Color(0xFF1a2649),
@@ -19,7 +24,7 @@ class MyApp extends StatelessWidget {
         textTheme: const TextTheme(),
         useMaterial3: true,
       ),
-      home: const Scaffold(),
+      home: ForgotPassword(),
     );
   }
 }
---
+++ lib/reset_password/forgot_password.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+import 'otp_verification.dart';
+
+class ForgotPassword extends StatelessWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: unused_local_variable
+    final screenWidth = MediaQuery.of(context).size.width;
+    // ignore: unused_local_variable
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+          child: Padding(
+        padding:
+            EdgeInsets.only(left: 0.1 * screenWidth, right: 0.1 * screenWidth),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 0.15 * screenHeight,
+            ),
+            Text(
+              'Forgot password',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+            SizedBox(
+              height: 0.01 * screenHeight,
+            ),
+            Text('Enter your phone number to get the OTP',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400,color: Color(0xFF4B5768)),),
+            SizedBox(
+              height: 0.07 * screenHeight,
+            ),
+            Text('Phone number',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400,color: Color(0xFF4B5768)),),
+            SizedBox(
+              height: 0.01 * screenHeight,
+            ),
+            TextField(
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                hintStyle: TextStyle(color: Color(0xFF999DA3),fontWeight: FontWeight.w100),
+                enabledBorder: OutlineInputBorder(
+
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    borderSide: BorderSide(width: 1.5,color: Color(0xffD0D5DD))),
+                  border: OutlineInputBorder(
+
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      borderSide: BorderSide(width: 0.2)),
+                  hintText: "Phone number",),
+            ),
+            SizedBox(
+              height: 0.07 * screenHeight,
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => OtpVerification()),
+                );
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    border: Border.all(),
+                    borderRadius: BorderRadius.all(Radius.circular(15))),
+                height: 0.06 * screenHeight,
+                width: double.infinity,
+                child: Center(
+                  child: const Text(
+                    'Forget',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
